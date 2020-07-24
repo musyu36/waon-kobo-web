cf982a15d7301a4a0af5507090273b26ff11e4a3
mimura <musyu36@gmail.com>
2020-07-24 17:58:21 +0900

    feat: project作成時のnotificationをデータベースに追加

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello, ninjas!");
+});
+
+// notificationsコレクションにnotificationオブジェクトを追加
+const createNotification = (notification) => {
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then((doc) => console.log("notification added", doc));
+};
+
+// projectが作成されたタイミングがトリガー
+exports.projectCreated = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate((doc) => {
+    const project = doc.data();
+    const notification = {
+      content: "Added a new project",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    //   Cloud Functions用の関数は何かしらのレスポンスが必要なのでreturnする形で関数を呼び出す
+    return createNotification(notification);
+  });
