e2abb9a12789cc142ffa933125042ac380e4f91f
mimura <musyu36@gmail.com>
2020-07-24 12:02:23 +0900

    feat: ログイン、ログアウト機能追加

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
+import { connect } from "react-redux";
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to="/" className="brand-logo left">
+        <Link to="/" className="brand-logo">
           WaonKobo
         </Link>
         <SignedInLinks />
@@ -17,4 +18,9 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {};
+};
+
+export default connect(mapStateToProps)(Navbar);
---
+++ src/store/actions/authActions.js
@@ -0,0 +1,30 @@
+export const signIn = (credentials) => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    //   singINWithEmailAndPasswordでログイン
+    //   ログインしたらthenの中でdispatch
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: "LOGIN_SUCCESS" });
+      })
+      .catch((err) => {
+        dispatch({ type: "LOGIN_ERROR" });
+      });
+  };
+};
+
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: "SIGNOUT_SUCCESS" });
+      });
+  };
+};
