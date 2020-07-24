a35173e28e0c782e0936621e7560b46cd7a98ccd
mimura <musyu36@gmail.com>
2020-07-24 14:11:32 +0900

    feat: ログイン、ログアウト時のルートガードを追加

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,10 +4,13 @@ import ProjectList from "../projects/ProjectList";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    const { projects } = this.props;
+    const { projects, auth } = this.props;
+    //   ルートガード
+    if (!auth.uid) return <Redirect to="/signin" />;
 
     return (
       <div className="dashboard container">
@@ -29,6 +32,7 @@ const mapStateToProps = (state) => {
   console.log(state);
   return {
     projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth,
   };
 };
 
--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createProject } from "../../store/actions/projectActions";
+import { Redirect } from "react-router-dom";
 
 class CreateProject extends Component {
   state = {
@@ -17,6 +18,10 @@ class CreateProject extends Component {
     this.props.createProject(this.state);
   };
   render() {
+    const { auth } = this.props;
+    // ルートガード
+    if (!auth.uid) return <Redirect to="/signin" />;
+
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -42,6 +47,12 @@ class CreateProject extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
+
 // createProjectとしてpropsに渡す
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -49,4 +60,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(CreateProject);
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
